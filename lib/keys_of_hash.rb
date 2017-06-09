490b64d59c5ac3b0a4201b17bb1755ab5c5bd5d2
Jevaughn McKenzie <jmckenzie1004@gmail.com>
2017-06-09 17:44:33 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,25 @@
+require "pry"
+
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+
+    relevant_keys = []
+
+    #compare arguments to values in hash
+    array_of_matching_values = self.values & arguments
+
+   #select keys of matching values
+
+    relevant_hash = self.select do |key, value|
+
+                      array_of_matching_values.include?(value)
+
+                      # relevant_keys << self.key(value)
+
+                    end
+
+  relevant_hash.keys
+
   end
 end
